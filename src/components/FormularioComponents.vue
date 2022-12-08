07a4ea8d6f8e2924923d43ad0e88d47b69794798
Julio-Hdot <juliohenrique1909@gmail.com>
2022-12-08 13:22:23 -0300

    adicionando a lib Maska e implementando-a

--- src/components/FormularioComponents.vue
+++ src/components/FormularioComponents.vue
@@ -6,40 +6,40 @@
                 <hr>
                 <form>
                     <div class="mb-3 row">
-                        <label class="col-3 col-form-label">Nome:</label>
+                        <label class="col-3 col-form-label" >Nome:</label>
                         <div class="col">
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" v-model="client.nome">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">E-mail:</label>
                         <div class="col">
-                            <input type="email" class="form-control">
+                            <input type="email" class="form-control" v-model="client.email">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Senha:</label>
                         <div class="col">
-                            <input type="password" class="form-control">
+                            <input type="password" class="form-control" v-model.trim="client.password">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Idade:</label>
                         <div class="col">
-                            <input type="number" class="form-control">
+                            <input type="number" class="form-control" v-model.number="client.idade">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Gênero:</label>
                         <div class="col">
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio">
+                                <input class="form-check-input" type="radio" value="Feminino" v-model="client.genero">
                                 <label class="form-check-label">
                                     Feminino
                                 </label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio">
+                                <input class="form-check-input" type="radio" value="Masculino" v-model="client.genero">
                                 <label class="form-check-label">
                                     Masculino
                                 </label>
@@ -51,7 +51,10 @@
                         <label class="col-3 col-form-label">Licença:</label>
                         <div class="col">
                             <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox">
+                                <input class="form-check-input" type="checkbox" 
+                                v-model="client.licenca"
+                                true-value="SIM"
+                                false-value="NÃO">
                                 <label class="form-check-label">Li e aceito os termos</label>
                             </div>
                         </div>
@@ -61,25 +64,25 @@
                         <label class="col-3 col-form-label">Interesses:</label>
                         <div class="col">
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox">
+                                <input class="form-check-input" type="checkbox" value="JavaScript" v-model="client.interesses" >
                                 <label class="form-check-label">
-                                    JavaScriot
+                                    JavaScript
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox">
+                                <input class="form-check-input" type="checkbox" value="VueJS" v-model="client.interesses">
                                 <label class="form-check-label">
                                     VueJS
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox">
+                                <input class="form-check-input" type="checkbox" value="Angular" v-model="client.interesses">
                                 <label class="form-check-label">
                                     Angular
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox">
+                                <input class="form-check-input" type="checkbox" value="NodeJS" v-model="client.interesses">
                                 <label class="form-check-label">
                                     NodeJS
                                 </label>
@@ -89,8 +92,101 @@
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Telefone:</label>
                         <div class="col">
-                            <input type="tel" class="form-control" pattern="[0-9]{2} [0-9]{5}-[0-9]{4}">
+                            <input type="text" class="form-control" v-model="client.telefone" v-maska="'(##) # ####-####'">
                             <small class="text-muted">Formato: 11 97777-5555</small>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">CEP:</label>
+                        <div class="col">
+                            <input type="text" class="form-control" v-model="client.cep" v-maska="'#####-###'">
+                            <small class="text-muted">Formato: 03610-256</small>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">RG:</label>
+                        <div class="col">
+                            <input type="text" class="form-control" v-model="client.rg" v-maska="'##.###.###-X'">
+                            <small class="text-muted">Formato: Sem padrão</small>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">CNPJ/CPF:</label>
+                        <div class="col">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" value="cpf" v-model="client.cnpj_cpf">
+                                <label class="form-check-label">
+                                    CPF
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" value="cnpj" v-model="client.cnpj_cpf">
+                                <label class="form-check-label">
+                                    CNPJ
+                                </label>
+                            </div>
+                        </div>
+                        <div v-show="client.cnpj_cpf == 'cnpj'">
+                            <div class="mb-3 row">
+                                <label class="col-3 col-form-label">CNPJ:</label>
+                                <div class="col">
+                                    <input type="text" class="form-control" v-model="client.cnpj" v-maska="'##.###.###/####-##'">
+                                    <small class="text-muted">Formato: 00.000.000/0000-00</small>
+                                </div>
+                            </div>
+                        </div>
+                        <div v-show="client.cnpj_cpf == 'cpf'">
+                            <div class="mb-3 row">
+                                <label class="col-3 col-form-label">CPF:</label>
+                                 <div class="col">
+                                    <input type="text" class="form-control" v-model="client.cpf" v-maska="'###.###.###-##'">
+                                    <small class="text-muted">Formato: 000.000.000-00</small>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Tipo Placa:</label>
+                        <div class="col">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" value="mercosul" v-model="client.tipoVeiculo">
+                                <label class="form-check-label">
+                                    Merco Sul
+                                </label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" value="antigo" v-model="client.tipoVeiculo">
+                                <label class="form-check-label">
+                                    Antigo
+                                </label>
+                            </div>
+                        </div>
+                        <div v-show="client.tipoVeiculo == 'mercosul'">
+                            <div class="mb-3 row">
+                                <label class="col-3 col-form-label">Merco Sul:</label>
+                                <div class="col">
+                                    <input type="text" class="form-control" v-model="client.placaVeiculoMecoSul" v-maska="'AAA#A##'">
+                                    <small class="text-muted">Formato: AAA0A00</small>
+                                </div>
+                            </div>
+                        </div>
+                        <div v-show="client.tipoVeiculo == 'antigo'">
+                            <div class="mb-3 row">
+                                <label class="col-3 col-form-label">Antigo:</label>
+                                 <div class="col">
+                                    <input type="text" class="form-control" v-model="client.placaVeiculo" v-maska="'AAA-####'">
+                                    <small class="text-muted">Formato: AAA-0000</small>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-3 col-form-label">Cartão:</label>
+                        <div class="col">
+                            <input type="text" class="form-control" v-model="client.cartaoCredito" v-maska="'####.####.####.####'">
+                            <small class="text-muted">Formato: 0000.0000.0000.0000</small>
                         </div>
                     </div>
                     <div class="mb-3 row">
@@ -164,61 +260,79 @@
                 <span class="fs-4">ESTADO DO OBJETO</span>
                 <hr>
                 <div class="mb-5 row">
-                    <spam>Estado do objeto</spam>
+                    <span>{{client}}</span>
                 </div>
 
                 <span class="fs-4">SAÍDA DE DADOS</span>
                 <hr>
                 <div class="mb-3 row">
-                    <spam>Nome:</spam>
+                    <span>Nome: {{client.nome}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>E-mail:</spam>
+                    <span>E-mail: {{client.email}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Senha:</spam>
+                    <span>Senha: {{client.password}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Idade:</spam>
+                    <span>Idade: {{client.idade}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Gênero:</spam>
+                    <span>Gênero: {{client.genero}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Licença:</spam>
+                    <span>Licença: {{client.licenca}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Interesses:</spam>
+                    <span>Interesses:</span>
+                    <ul>
+                        <li v-for="(tech, index) in client.interesses" :key="index">{{tech}}</li>
+                    </ul>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Telefone:</spam>
+                    <span>Telefone: {{client.telefone}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Data:</spam>
+                    <span>CEP: {{client.cep}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Data/hora local:</spam>
+                    <span>CNPJ: {{client.cnpj}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Mês:</spam>
+                    <span>Cartão de Credito: {{client.cartaoCredito}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Semana:</spam>
+                    <span>Placa Veiculo: {{client.placaVeiculo}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Hora:</spam>
+                    <span>Placa Veiculo MecoSul: {{client.placaVeiculoMecoSul}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Cor:</spam>
+                    <span>RG: {{client.rg}}</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Valor limite:</spam>
+                    <span>Data/hora local:</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Escondido:</spam>
+                    <span>Mês:</span>
                 </div>
                 <div class="mb-3 row">
-                    <spam>Upload:</spam>
+                    <span>Semana:</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Hora:</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Cor:</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Valor limite:</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Escondido:</span>
+                </div>
+                <div class="mb-3 row">
+                    <span>Upload:</span>
                 </div>  
             </div>
         </div>
@@ -229,6 +343,28 @@
 
 <script>
 export default {
-    name: 'Formulario'
+    name: 'Formulario',
+    data:()=>({
+        
+        client:{
+            nome:'',
+            email:'',
+            password:'',
+            idade:'',
+            licenca: 'NÃO',
+            interesses: [],
+            genero:'',
+            telefone: '',
+            cep:'',
+            cpf:'',
+            cnpj:'',
+            cnpj_cpf: '',
+            cartaoCredito:'',
+            placaVeiculo:'',
+            placaVeiculoMecoSul:'',
+            tipoVeiculo:'',
+            rg:'',
+        }
+    })
 }
 </script>
